a5d653bebbad7888a407396bf5ebc2277d66e84e
Juan Felipe Castellanos Bran <juanfelipecastellanosbran@gmail.com>
2026-09-07 20:55:29 -0500

    Agrega Main del sistema de sensores

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Sistema de Red de Sensores IoT");
+        System.out.println("Iniciando procesamiento...");
+
+    }
+}
